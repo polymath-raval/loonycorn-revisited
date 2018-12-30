d1c40eb1b58a4acfc324bd986b267804b0de3134
Nikunj Raval <polymath.raval@gmail.com>
2018-12-30 17:48:34 +0530

    Adding search code

--- src/main/java/edu/raval/polymath/loonycorn/revisited/sort/sort0/ISort.java
+++ src/main/java/edu/raval/polymath/loonycorn/revisited/sort/sort0/ISort.java
@@ -1,5 +1,21 @@
 package edu.raval.polymath.loonycorn.revisited.sort.sort0;
 
 public interface ISort {
+
+  static int search(int searchParam, int[] sortedArr, int startIndex, int endIndex) {
+    if(startIndex >  endIndex){
+      return -1;
+    }
+    int midIndex = (startIndex + endIndex) / 2;
+    int midValue = sortedArr[midIndex];
+    if (searchParam == midValue) {
+      return midIndex;
+    } else if (searchParam < midValue) {
+      return search(searchParam, sortedArr, startIndex, midIndex-1);
+    } else {
+      return search(searchParam, sortedArr, midIndex + 1, endIndex);
+    }
+  }
+
   int[] sort(int[] input);
 }
--- src/test/java/edu/raval/polymath/loonycorn/revisited/sort/sort0/ISortTest.java
+++ src/test/java/edu/raval/polymath/loonycorn/revisited/sort/sort0/ISortTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ISortTest {
+
   /*private static int range = 100_000_000;
   private static int size = 100_000_000;
   */
@@ -14,42 +15,51 @@ public class ISortTest {
   private static int[] input;
 
   @Test
-  public void MergeSort(){
+  public void MergeSort() {
     outOfPlaceSort(new MergeSort());
   }
 
   @Test
-  public void QuickSort(){
+  public void QuickSort() {
     inplaceSort(new QuickSort());
   }
 
   @Test
-  public void SelectionSort(){
+  public void SelectionSort() {
     inplaceSort(new SelectionSort());
   }
 
   @Test
-  public void BubbleSort(){
+  public void BubbleSort() {
     inplaceSort(new BubbleSort());
   }
 
   @Test
-  public void InsertionSort(){
+  public void InsertionSort() {
     inplaceSort(new InsertionSort());
   }
 
   @Test
-  public void ShellSort(){
+  public void ShellSort() {
     inplaceSort(new ShellSort());
   }
 
-  private void outOfPlaceSort(ISort sort){
+  @Test
+  public void Search() {
+    int[] _input = initArray(size);
+    new QuickSort().sort(_input);
+
+    for (int searchParam : input) {
+      int searchIndex = ISort.search(searchParam, _input, 0, _input.length);
+      Assert.assertTrue(_input[searchIndex] == searchParam);
+    }
+  }
+
+  private void outOfPlaceSort(ISort sort) {
     int[] input = initArray(size);
-    //System.out.println(Arrays.toString(input));
     int[] output = sort.sort(input);
-    //System.out.println(Arrays.toString(output));
     int last = Integer.MIN_VALUE;
-    for(int i:output){
+    for (int i : output) {
       Assert.assertTrue(last <= i);
       last = i;
     }
@@ -57,18 +67,16 @@ public class ISortTest {
 
   private void inplaceSort(ISort sort) {
     int[] input = initArray(size);
-    //System.out.println(Arrays.toString(input));
     sort.sort(input);
-    //System.out.println(Arrays.toString(input));
     int last = Integer.MIN_VALUE;
-    for(int i:input){
+    for (int i : input) {
       Assert.assertTrue(last <= i);
       last = i;
     }
   }
 
-  private int[] initArray(int size){
-    if(input == null){
+  private int[] initArray(int size) {
+    if (input == null) {
       System.out.println("Initialize Array");
       input = new int[size];
       for (int i = 0; i < input.length; i++) {
